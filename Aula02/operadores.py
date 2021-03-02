95e00d15c25a93d76d069c6065ba4fdf842b8d1d
Evaldo-comp <evaldocomp@gmail.com>
2021-03-01 21:59:28 -0300

    Aula02

---
+++ Aula02/operadores.py
@@ -0,0 +1,55 @@
+# Operadores
+# Ariméticos
+# Atribuição
+# Comparação
+# Lógicos
+# Relacionais
+
+# Aritméticos
+
+soma = 2 + 2
+sub = 3 - 2
+mul = 3 * 3
+div = 9 / 3   # divisão retorna um float
+div2 = 9 // 3  # divisão retorna um inteiro
+mod = 10 % 3
+pote = 2 ** 3
+
+# Operadores de Atribuição
+num1 = 2
+num2 = 3
+
+num1 += num2  # num1 = num1 + num2
+num1 -= num2  # num1 = num1 - num2
+num1 *= num2   # num1 = num1 * num2
+num1 /= num2   # num1 = num1 / num2
+num1 %= num2   # num1 = num1 % num2
+num1 **= num2   # num1 = num1 ** num2
+
+
+# operadores de comparação
+'''
+print(num1 == num2)
+print(num1 != num2)
+print(num1 >= num2)
+print(num1 <= num2)
+print(num1 > num2)
+print(num1 < num2)
+'''
+
+# Operadores Lógicos
+
+# and = E
+# or = OU
+# not = Não
+'''
+num5 = 2
+num6 = 3
+print((num5 == num6) or (False)) # False
+print((num5 != num6) and (num5 == 2))  # True
+print(not(num5 == 3))
+'''
+
+# operadores de associação
+nome = "Francisco Evaldo"
+print(nome.up())
